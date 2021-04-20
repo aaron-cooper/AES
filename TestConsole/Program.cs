acd9a4aad08586f908f7108e4bf1a656370ee1ef
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-04-20 00:58:48 -0400

    Add project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AES;
+using System.Collections.Generic;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<byte> set = new HashSet<byte>();
+            for (int i= 0; i< 256; i++)
+            {
+                byte b = (byte)i; 
+                byte ab = RijndaelSubBox.Sub(b);
+                set.Add(ab);
+
+                if (b != RijndaelSubBox.SubInverse(ab))
+                {
+                    Console.WriteLine("incorrect inverse");
+                }
+            }
+            if (set.Count != 256)
+            {
+                Console.WriteLine("missing inverse");
+            }
+        }
+    }
+}
