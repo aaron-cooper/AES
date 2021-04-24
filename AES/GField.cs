d177f1eddda649f242222e16e37e85a72831ea2a
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-04-23 22:02:50 -0400

    Add class for GField operations.
    Currenlty contains a function for multiplying 2 values in GF(2 ^ 8). Multiplies numbers as described in fips 197 sec 4.2 and 4.2.1

---
+++ AES/GField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AES
+{
+    public class GField
+    {
+        /// <summary>
+        /// Multiply two values in GF(2^8)
+        /// </summary>
+        public static byte Mult(byte a, byte b)
+        {
+            // peasant multiplication, reduce as described in fips 197
+            byte product = 0;
+            while(a > 0)
+            {
+                if ((a & 1) != 0)
+                {
+                    product ^= b;
+                }
+
+                // fips 197 says to reduce if bit 7 is set
+                // bit 7 will carry out so test it before
+                bool shouldReduce = (b & 0x80) != 0;
+                b <<= 1;
+                if (shouldReduce)
+                {
+                    b ^= 0x1b;
+                }
+                a >>= 1;
+            }
+            return product;
+        }
+    }
+}
