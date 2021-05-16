10588703c71877dfaba51b761256516ac7e4997d
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-05-15 21:26:26 -0400

    stub out AES class

---
+++ AES/AES.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AES
+{
+    public class AES : IDisposable
+    {
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+
+        private RandomNumberGenerator rng;
+
+        private AES()
+        {
+            using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            this.GenerateKey();
+            this.GenerateIV();
+        }
+
+        public ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateIV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
