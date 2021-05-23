5b02ca8ce851d56fff0bfbb56436f11fa9653968
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-05-23 17:22:29 -0400

    stub out ICryptoTransform implementation in AESDecryptor

--- AES/AESDecryptor.cs
+++ AES/AESDecryptor.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace AES
 {
-    class AESDecryptor
+    class AESDecryptor : ICryptoTransform
     {
+        public bool CanReuseTransform => throw new NotImplementedException();
+
+        public bool CanTransformMultipleBlocks => throw new NotImplementedException();
+
+        public int InputBlockSize => throw new NotImplementedException();
+
+        public int OutputBlockSize => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
