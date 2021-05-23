b617a13120d5a1edf5beb5297706dd7dc18593cc
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-05-23 19:45:34 -0400

    stub out AESEncryptor

---
+++ AES/AESEncryptor.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+
+namespace AES
+{
+    class AESEncryptor : ICryptoTransform
+    {
+        public bool CanReuseTransform => true;
+
+        public bool CanTransformMultipleBlocks => true;
+
+        public int InputBlockSize => AES.ValidBlockSize;
+
+        public int OutputBlockSize => AES.ValidBlockSize;
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
