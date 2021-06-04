49c8a4fa71ab29434dfaa3e25ec28679324969dc
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2021-06-04 17:02:50 -0400

    Add test for creating decryptor

--- AESUnitTesting/AESDecryptorTests.cs
+++ AESUnitTesting/AESDecryptorTests.cs
@@ -7,6 +7,15 @@ namespace AESUnitTesting
 {
     class AESDecryptorTests
     {
+        const int blocksize = 16;
+        private byte[] Key = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+        private byte[] IV = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
+        [Test]
+        public void Test_CreateDecryptor()
+        {
+            AES.AES aes = new AES.AES(Key, IV);
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+        }
     }
 }
